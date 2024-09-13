efa974065b6cb3a756d294167cd339013e2ded50
Kartik Agnihotri <agni5kartik@gmail.com>
2024-09-13 19:36:08 +0530

    Update snippet.js

--- snippet.js
+++ snippet.js
@@ -47,8 +47,7 @@ function saveContent(querySelector, content, url) {
 
 function applySavedContent() {
   const url = window.location.href;
-  const bodyElement = document.getElementsByTagName("body")[0];
-  bodyElement.querySelectorAll('*').forEach(element => {
+  document.getElementsByTagName("body")[0].querySelectorAll('*').forEach(element => {
     const querySelector = getQuerySelector(element);
     fetch('http://localhost:4999/get_saved_content', {
       method: 'POST',
